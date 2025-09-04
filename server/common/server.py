4b54e06939d316d05b7c58729e10e1acd747bf8c
mvalmaggia <mvalmaggia@fi.uba.ar>
2025-09-04 17:18:43 -0300

    add threads to server

--- server/common/server.py
+++ server/common/server.py
@@ -1,7 +1,7 @@
 import socket
 import logging
 import signal
-
+import threading
 import time
 
 from codec.codec import decode_bet_batch, encode_winners
@@ -10,9 +10,13 @@ from common.utils import store_bets, load_bets, has_won
 
 class Server:
     def __init__(self, port, listen_backlog, expected_agencies=5):
-        # Initialize server socket
+        
+        self._client_threads = []
+        self._lock = threading.Lock()
+
         self._finished_agencies = set()
         self._expected_agencies = expected_agencies
+
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
@@ -38,8 +42,24 @@ class Server:
         while self._running:
             self._client_sock = self.__accept_new_connection()
             if self._client_sock:
-                self.__handle_client_connection(self._client_sock)
+                t = threading.Thread(
+                    target=self.__handle_client_connection, 
+                    args=(self._client_sock,)
+                )
+                t.start()
+                self._client_threads.append(t)
                 self._client_sock = None
+            
+            alive_threads = []
+            for t in self._client_threads:
+                if t.is_alive():
+                    alive_threads.append(t)
+                else:
+                    t.join()
+            self._client_threads = alive_threads
+
+        for t in self._client_threads:
+            t.join()
         self._shutdown()
 
     def _handle_sigterm(self, signum, frame):
@@ -62,14 +82,16 @@ class Server:
         Retrieves dictiornary with agency ids as keys and list of document ids as values
         for all the winning bets stored in the STORAGE_FILEPATH file.
         """
-        self._winners_by_agency = {}
-        bets = load_bets()
-        for bet in bets:
-            if has_won(bet):
-                agency = bet.agency
-                if agency not in self._winners_by_agency:
-                    self._winners_by_agency[agency] = []
-                self._winners_by_agency[agency].append(bet.document)
+        with self._lock:
+            self._winners_by_agency = {}
+    
+            bets = load_bets()
+            for bet in bets:
+                if has_won(bet):
+                    agency = bet.agency
+                    if agency not in self._winners_by_agency:
+                        self._winners_by_agency[agency] = []
+                    self._winners_by_agency[agency].append(bet.document)
 
     def _get_winners_for_agency(self, agency_id: int):
         """
@@ -89,14 +111,17 @@ class Server:
         logging.info(f"action: send_winners | result: success | agency: {agency_id}")
 
     def _handle_batch_bet(self, encoded_msg):
-        bets = decode_bet_batch(encoded_msg) #TODO: SALTEAR TITULO BET_BATCH
+        bets = decode_bet_batch(encoded_msg)
 
-        store_bets(bets)
+        with self._lock:
+            store_bets(bets)
+
         logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)}')
 
     def _handle_batch_end(self, agency_id):
-        self._finished_agencies.add(agency_id)
-        logging.info(f"action: batch_end_received | result: success | agency: {agency_id}")
+        with self._lock:
+            self._finished_agencies.add(agency_id)
+            logging.info(f"action: batch_end_received | result: success | agency: {agency_id}")
 
         if len(self._finished_agencies) == self._expected_agencies:
             logging.info("action: sorteo | result: success")
